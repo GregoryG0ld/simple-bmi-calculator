dfd1f9359cad250b8c2a56406b55d3d965db6d03
Gregory <gregory@web-zeus.com>
2023-12-22 11:27:08 +0200

    commit

---
+++ js/lang_en.js
@@ -0,0 +1,9 @@
+const lang_en = {
+   title: 'Body Mass Index',
+   description: 'The Body Mass Index (BMI) Calculator can be utilized to compute the BMI value and determine the corresponding weight status.',
+   heightLabel: 'Please Enter your Height in meters',
+   weightLabel: 'Please Enter your Weight in kg',
+   calculateBtn: 'Calculate',
+   clearBtn: 'Clear',
+   startCalculating: 'Start Calculating!',
+};
---
+++ js/lang_lt.js
@@ -0,0 +1,9 @@
+const lang_lt = {
+   title: 'Kūno masės indeksas',
+   description: 'Kūno masės indekso (KMI) skaičiuoklė gali būti naudojama KMI reikšmei apskaičiuoti ir atitinkamai svorio būsenai nustatyti.',
+   heightLabel: 'Įveskite savo aukštį metrais',
+   weightLabel: 'Įveskite savo svorį kg',
+   calculateBtn: 'Calculate',
+   clearBtn: 'Apskaičiuoti',
+   startCalculating: 'Pradėkite skaičiuoti!',
+};
